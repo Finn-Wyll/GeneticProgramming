6b4652200e23187814a6984ea2d45bd5d44df005
Finnley Wyllie <u24754120@tuks.co.za>
2026-05-31 00:49:11 +0200

    added overloaded method to initialise the population from a text file

--- GeneticProgram.java
+++ GeneticProgram.java
@@ -28,7 +28,6 @@ public abstract class GeneticProgram {
 		this.random = new Random(seed);
 	}
 
-	
 	protected abstract void initialise();
 
 	protected abstract double evaluateFitness(Node<Double> individual, double[][] X, int[] y);
@@ -41,8 +40,72 @@ public abstract class GeneticProgram {
 
 	protected abstract int predict(Node<Double> tree, double[] features);
 
+	protected abstract void initialiseByPath(String path);
+
 	public Node<Double> train(double[][] X, int[] y) {
 		return train(X, y, false);
+	}
+
+	public Node<Double> train(double[][] X, int[] y, String path) {
+		return train(X, y, false, path);
+	}
+
+	public Node<Double> train(double[][] X, int[] y, boolean verbose, String path) {
+		initialiseByPath(path);
+
+		bestFitness = -1;
+
+		for (int gen = 0; gen < maxGenerations; gen++) {
+
+			// Evaluate fitness and track best individual
+			double[] fitnesses = new double[populationSize];
+			for (int i = 0; i < populationSize; i++) {
+				fitnesses[i] = evaluateFitness(population.get(i), X, y);
+				if (fitnesses[i] > bestFitness) {
+					bestFitness = fitnesses[i];
+					bestIndividual = population.get(i).clone();
+				}
+			}
+
+			if (verbose) {
+				System.out.printf("Generation %d | Best fitness: %.4f | Best individual: %s%n",
+						gen, bestFitness, describe(bestIndividual));
+			}
+
+			if (bestFitness == 1.0)
+				break;
+
+			// Selection, crossover, mutation
+			List<Node<Double>> offspring = new ArrayList<>();
+			while (offspring.size() < populationSize) {
+				Node<Double> parent1 = tournamentSelect(fitnesses);
+				Node<Double> parent2 = tournamentSelect(fitnesses);
+
+				Node<Double> child1, child2;
+
+				if (random.nextDouble() < crossoverRate) {
+					List<Node<Double>> children = crossover(parent1, parent2);
+					child1 = children.get(0);
+					child2 = children.get(1);
+				} else {
+					child1 = parent1.clone();
+					child2 = parent2.clone();
+				}
+
+				if (random.nextDouble() < mutationRate)
+					child1 = mutate(child1);
+				if (random.nextDouble() < mutationRate)
+					child2 = mutate(child2);
+
+				offspring.add(child1);
+				offspring.add(child2);
+			}
+
+			// Trim population size
+			population = offspring.subList(0, populationSize);
+		}
+
+		return bestIndividual;
 	}
 
 	public Node<Double> train(double[][] X, int[] y, boolean verbose) {
